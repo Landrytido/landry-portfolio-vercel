02d867c351a84acb48dae36109a0e89bee8604e7
Landrytido <atla2804@student.eafc-uccle.be>
2025-04-08 00:11:59 +0200

    Mise à jour de l'icône du site et ajout du fichier atl-logo.svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/atl-logo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.variable}>
